06d5d9bc46c7951234a20f48bd1d4466dcdd7f17
PC33-IFLAB <PC33-IFLAB>
2023-03-13 07:32:33 +0700

    Initial commit

---
+++ MOD_4_1302210106/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace MOD_4_1302210106
+{
+    class KodeBuah
+    {
+        Dictionary<String, String> dic_buah = new Dictionary<string, String>()
+        {
+            {"Apel","A00" },
+            {"Aprikot", "B00"},
+            {"Alpukat", "C00"},
+            {"Pisang", "D00"},
+            {"Paprika", "E00"},
+            {"Blackberry", "F00"},
+            {"Ceri", "H00"},
+            {"Kelapa", "I00"},
+            {"jagung", "J00"},
+            {"Kurma", "K00"},
+            {"Durian", "L00"},
+            {"Angggur", "M00"},
+            {"Melon", "N00"},
+            {"Semangka", "O00"},
+        };
+
+        public void getKodeBuah(String Buah)
+        {
+            if (dic_buah.ContainsKey(Buah))
+            {
+                Console.WriteLine(Buah + " : " + dic_buah[Buah]);
+            }
+            else
+            {
+                Console.WriteLine("Kode dari Buah"+ Buah + " tidak terdaftar");
+            }
+        }
+
+
+    }
+
+
+    class Mainprogram
+    {
+        public static void Main(String[] args)
+        {
+            KodeBuah kode = new KodeBuah();
+            Console.Write("Masukan Buah yang ingin dilihat Kode nya : ");
+            String buah = Console.ReadLine();
+            kode.getKodeBuah(buah);
+        }
+    }
+
+
+
+
+
+
+}
